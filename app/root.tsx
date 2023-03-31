850fbd3c169cc50706481ff64b9bab1c8ce56de7
kaaax0815 <999999bst@gmail.com>
2023-03-31 17:28:52 +0200

    move to v2 routing

--- app/root.tsx
+++ app/root.tsx
@@ -1,15 +1,19 @@
-import type { MetaFunction } from '@remix-run/node';
+import type { V2_MetaFunction } from '@remix-run/node';
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 
-export const meta: MetaFunction = () => ({
-  charset: 'utf-8',
-  title: 'Abiball',
-  viewport: 'width=device-width,initial-scale=1'
-});
+export const meta: V2_MetaFunction = () => {
+  return [
+    {
+      charSet: 'utf-8'
+    },
+    { title: 'Abiball' },
+    { name: 'viewport', content: 'width=device-width,initial-scale=1' }
+  ];
+};
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="de">
       <head>
         <Meta />
         <Links />
--- remix.config.js
+++ remix.config.js
@@ -2,8 +2,13 @@
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
   server: process.env.NETLIFY || process.env.NETLIFY_LOCAL ? './server.js' : undefined,
-  serverBuildPath: '.netlify/functions-internal/server.js'
+  serverBuildPath: '.netlify/functions-internal/server.js',
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
+  future: {
+    v2_routeConvention: true,
+    v2_meta: true,
+    v2_errorBoundary: true
+  }
 };
