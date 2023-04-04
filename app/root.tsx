d8e57984d32454798043e3f28d9e8d63d307914f
kaaax0815 <999999bst@gmail.com>
2023-04-04 22:59:26 +0200

    self host fonts

--- app/root.tsx
+++ app/root.tsx
@@ -3,17 +3,7 @@ import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@re
 
 import stylesheet from '~/tailwind.css';
 
-export const links: LinksFunction = () => [
-  { rel: 'stylesheet', href: stylesheet },
-  {
-    rel: 'preconnect',
-    href: 'https://fonts.bunny.net'
-  },
-  {
-    rel: 'stylesheet',
-    href: 'https://fonts.bunny.net/css?family=inter:400'
-  }
-];
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: stylesheet }];
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: 'Abiball' }];
