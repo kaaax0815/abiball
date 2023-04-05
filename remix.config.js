8f22b552a7f32fe7b55272e241a63948ae8b92b2
kaaax0815 <999999bst@gmail.com>
2023-04-05 16:36:38 +0200

    WIP: use remix-flat-routes

--- remix.config.js
+++ remix.config.js
@@ -1,16 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { flatRoutes } = require('remix-flat-routes');
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ['**/.*'],
+  ignoredRouteFiles: ['**/*'],
   server: process.env.NETLIFY || process.env.NETLIFY_LOCAL ? './server.js' : undefined,
   serverBuildPath: '.netlify/functions-internal/server.js',
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
   future: {
     v2_routeConvention: true,
     v2_meta: true,
     v2_errorBoundary: true,
     unstable_tailwind: true,
     v2_normalizeFormMethod: true
+  },
+  routes: async (defineRoutes) => {
+    return flatRoutes('routes', defineRoutes);
   }
 };
