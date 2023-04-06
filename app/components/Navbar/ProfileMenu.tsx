f4e238d19def8e0db17f8cb3a05e3c209afb928a
kaaax0815 <999999bst@gmail.com>
2023-04-06 23:09:26 +0200

    add profile route recognition

--- app/components/Navbar/ProfileMenu.tsx
+++ app/components/Navbar/ProfileMenu.tsx
@@ -1,6 +1,6 @@
 import { Menu, Transition } from '@headlessui/react';
 import { ArrowLeftOnRectangleIcon, UserIcon } from '@heroicons/react/24/outline';
-import { Form, Link } from '@remix-run/react';
+import { Form, Link, NavLink } from '@remix-run/react';
 import { Fragment } from 'react';
 
 export type ProfileMenuProps = {
@@ -41,17 +41,17 @@ export default function ProfileMenu({ user }: ProfileMenuProps) {
             <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none">
               <Menu.Item>
                 {({ active }) => (
-                  <Link
+                  <NavLink
                     to="/profile"
                     className={
-                      'block w-full text-left px-2 py-2 text-sm text-gray-700' +
+                      'block w-full text-left px-2 py-2 text-sm text-gray-700 aria-active:bg-gray-200' +
                       `${active ? ' bg-gray-100' : ''}`
                     }
                   >
                     <span>
                       <UserIcon className="inline-block h-6 w-6" /> Profil
                     </span>
-                  </Link>
+                  </NavLink>
                 )}
               </Menu.Item>
               <Menu.Item>
